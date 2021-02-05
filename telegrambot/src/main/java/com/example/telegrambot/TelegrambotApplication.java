efa5f10a1c96c007246fe0f7d6244e63156f7fa4
Erick Barroso De Souza <ericksouza@mobitbrasil.com.br>
2021-02-05 13:55:35 -0300

    configurando deploy

--- telegrambot/src/main/java/com/example/telegrambot/TelegrambotApplication.java
+++ telegrambot/src/main/java/com/example/telegrambot/TelegrambotApplication.java
@@ -2,15 +2,9 @@ package com.example.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import com.example.telegrambot.telegrambotconfig.GlpiTelegramMobitBot;
 
 @SpringBootApplication
-//@PropertySource("file:application.properties")
 public class TelegrambotApplication {
 
 	public static void main(String[] args) {
